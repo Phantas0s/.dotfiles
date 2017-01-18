5d6921369ab7b6e02706a620cdb3c176d93b8430
Matthieu <matthieu.cneude@lesara.de>
2017-01-18 15:30:31 +0100

    Test including bash script into bash script

---
+++ install-prezto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'Create symlinks for personnal prezto themes'
+
+ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_sorin2_setup $HOME/.zprezto/modules/prompt/functions/
+ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_garrett_setup $HOME/.zprezto/modules/prompt/functions/
+ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_garrett2_setup $HOME/.zprezto/modules/prompt/functions/
+
+
--- install.sh
+++ install.sh
@@ -66,9 +66,4 @@ ln -s -f $HOME/$ROOT_CONFIG/tmux/tmux.conf $HOME/.tmux.conf
 ln -s -f $HOME/$ROOT_CONFIG/urxvt/Xdefaults $HOME/.Xdefaults
 ln -s -f $HOME/$ROOT_CONFIG/urxvt/autostart/urxvtd.desktop $HOME/.config/autostart/
 
-echo 'Create symlinks for personnal prezto themes'
-
-ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_sorin2_setup $HOME/.zprezto/modules/prompt/functions/
-ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_garrett_setup $HOME/.zprezto/modules/prompt/functions/
-ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_garrett2_setup $HOME/.zprezto/modules/prompt/functions/
-
+. $HOME/$ROOT_CONFIG/install-prezto.sh
