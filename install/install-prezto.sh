6edb3c0bc601c3c7170542133e5a64af6d41fcad
Matthieu <matthieu.cneude@lesara.de>
2017-01-18 16:40:42 +0100

    garrett2 theme does not ls each time the directory is changed anymore

--- install/install-prezto.sh
+++ install/install-prezto.sh
@@ -8,9 +8,15 @@ git clone git@github.com:sorin-ionescu/prezto ~/.zprezto
 cd $HOME/.zprezto && git pull && git submodule update --init --recursive
 cd -
 
+echo 'Create symlinks for Prezto...'
+ln -s -f $HOME/.zprezto/runcoms/zlogin $HOME/.zlogin
+ln -s -f $HOME/.zprezto/runcoms/zlogout $HOME/.zlogout
+ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/config/zpreztorc $HOME/.zpreztorc
+ln -s -f $HOME/.zprezto/runcoms/zprofile $HOME/.zprofile
+ln -s -f $HOME/.zprezto/runcoms/zshenv $HOME/.zshenv
+ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/config/zshrc $HOME/.zshrc
 
 echo 'Create symlinks for personnal prezto themes'
-
 ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_sorin2_setup $HOME/.zprezto/modules/prompt/functions/
 ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_garrett_setup $HOME/.zprezto/modules/prompt/functions/
 ln -s -f $HOME/$ROOT_CONFIG/zsh/zprezto/themes/prompt_garrett2_setup $HOME/.zprezto/modules/prompt/functions/
