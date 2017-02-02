90c5ba6876c52f69ac732eb62da7a2b585290d4f
Matthieu <matthieu.cneude@lesra.de>
2017-02-02 16:18:56 +0100

    Add nvim configuration and verify if phpstorm shortcut exists

--- install/install-phpstorm.sh
+++ install/install-phpstorm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo ""
-echo "${blue}Try to create symlink for Phpstorm (in /usr/bin/phpstorm)..."
-
 if [ -z "$PHPSTORM_ROOT" ]
   then
-      echo "${red}... no path configured in $ROOT_CONFIG/install_config"
-else
+      echo "${red}... no path configured for PHPStorm in $ROOT_CONFIG/install_config"
+elif [ ! -e /usr/bin/phpstorm ]
+  then
+    echo ""
+    echo "${blue}Try to create symlink for Phpstorm (in /usr/bin/phpstorm)..."
     sudo ln -s -f $PHPSTORM_ROOT /usr/bin/phpstorm
     echo "${green}... done"
 fi
