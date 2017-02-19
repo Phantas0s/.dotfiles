c0e75a10c774bc6d042a4d82e733560e548ab2c3
Matthieu <Matthieu.cneude@gmail.com>
2017-02-19 21:40:40 +0100

    Minor fix

--- install/install-from-cloud.sh
+++ install/install-from-cloud.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "${blue} installing personnal configuration from the cloud..."
+echo "${blue} installing private projects configuration from the cloud..."
 
 if [[ -v DOTFILES_PROJECT_PATH ]]
 then
     ln -s -f $DOTFILES_PROJECT_PATH/.tmuxinator $HOME/
+    # project relative configuration for neovim
     ln -s -f $DOTFILES_PROJECT_PATH/projects.nvimrc $HOME/nvim/
 fi
 
+echo ""
 echo "${green} ...done"
