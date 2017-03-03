65fe2ca0ac8b551e1637af105ae383422ad8f4ec
Matthieu <Matthieu.cneude@gmail.com>
2017-03-04 00:04:02 +0100

    Rename tmuxinator hidden file for nextcloud

--- install/install-from-cloud.sh
+++ install/install-from-cloud.sh
@@ -4,7 +4,7 @@ echo "${blue} installing private projects configuration from the cloud..."
 
 if [[ -v DOTFILES_PROJECT_PATH ]]
 then
-    ln -s -f $DOTFILES_PROJECT_PATH/.tmuxinator $HOME/
+    ln -s -f $DOTFILES_PROJECT_PATH/tmuxinator $HOME/.tmuxinator
     # project relative configuration for neovim
     ln -s -f $DOTFILES_PROJECT_PATH/projects.nvimrc $HOME/nvim/
 fi
