b89e7c32cfccad90d94ef50d07c9190d8a4ec920
Matthieu <Matthieu.cneude@gmail.com>
2017-04-21 15:56:32 +0200

    Finishing to configure dunst

--- install/install-dunst.sh
+++ install/install-dunst.sh
@@ -1,7 +1,7 @@
 echo ""
 echo "${blue}Install dunst..."
 
-ln -s $HOME/$ROOT_CONFIG/dunst/dunstrc $HOME/dunstrc
+ln -s $HOME/$ROOT_CONFIG/dunst/dunstrc $HOME/.dunstrc
 ln -s $HOME/$ROOT_CONFIG/dunst/autotest $HOME/.autotest
 
 echo "${green} ...done"
