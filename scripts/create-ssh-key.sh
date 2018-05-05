d28154db095e721f65e708aaa8f95277d7d6d63a
matthieu <matthieu.cneude@gmail.com>
2018-05-05 14:52:11 +0200

    Improve script to create ssh keys

--- scripts/create-ssh-key.sh
+++ scripts/create-ssh-key.sh
@@ -2,7 +2,7 @@
 
 if [ ! -z "$1" ];
 then
-    ssh-keygen -f $HOME/.ssh/$1 -C "Key $1 added via create-ssh-key script"
+    ssh-keygen -f $HOME/.ssh/$1 -t rsa -N '' -C "Key $1 added via create-ssh-key script"
     chmod 700 $HOME/.ssh/$1*
 fi
 
