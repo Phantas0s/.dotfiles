d6f2d8b24e8a4a3dccb03a677b1a6e708038bd46
matthieu <matthieu.cneude@gmail.com>
2018-05-05 14:47:23 +0200

    Add install for projects after generating and linking ssh key to github account

---
+++ install/install-projects.sh
@@ -0,0 +1,34 @@
+# Clone projects from github / gitlab
+# The key have to be created manually of course...
+# Only Phantas0s have access to the repo via SSH :D
+
+if [ ! -f $HOME/.ssh/github ];
+    then
+        mkdir $GOPATH/src/github.com/Phantas0s > /dev/null
+
+        if [ ! -d $GOPATH/src/github.com/Phantas0s/watcher ];
+        then
+            echo -e "Installing the project Watcher in workspace"
+            cd $GOPATH/src/github.com/Phantas0s
+            git clone git@github.com:Phantas0s/watcher.git
+            go install
+            cd -
+        fi
+
+        if [ ! -d $GOPATH/src/github.com/Phantas0s/testomatic ];
+        then
+            echo -e "Installing the project Testomatic in workspace"
+            cd $GOPATH/src/github.com/Phantas0s
+            git clone git@github.com:Phantas0s/testomatic.git
+            go install
+            cd -
+        fi
+
+        if [ ! -d $GOPATH/src/github.com/Phantas0s/go-in-practice ];
+        then
+            echo -e "Installing the project Go in practice in workspace for reference"
+            cd $GOPATH/src/github.com/Phantas0s
+            git clone git@github.com:Phantas0s/go-in-practice.git
+            cd -
+        fi
+fi
--- scripts/create-ssh-key.sh
+++ scripts/create-ssh-key.sh
@@ -3,5 +3,6 @@
 if [ ! -z "$1" ];
 then
     ssh-keygen -f $HOME/.ssh/$1 -C "Key $1 added via create-ssh-key script"
+    chmod 700 $HOME/.ssh/$1*
 fi
 
