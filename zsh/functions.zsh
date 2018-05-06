c361f29becceff40d6411ee12770bbeaea8604f4
matthieu <matthieu.cneude@gmail.com>
2018-05-06 14:13:58 +0200

    Delete tmuxinator - use tmuxp now

---
+++ zsh/functions.zsh
@@ -0,0 +1,23 @@
+extract() {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)  tar xjf $1      ;;
+      *.tar.gz)   tar xzf $1      ;;
+      *.bz2)      bunzip2 $1      ;;
+      *.rar)      rar x $1        ;;
+      *.gz)       gunzip $1       ;;
+      *.tar)      tar xf $1       ;;
+      *.tbz2)     tar xjf $1      ;;
+      *.tgz)      tar xzf $1      ;;
+      *.zip)      unzip $1        ;;
+      *.Z)        uncompress $1   ;;
+      *)          echo "'$1' cannot be extracted" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+compress() {
+    tar cvzf $1.tar.gz $1
+}
