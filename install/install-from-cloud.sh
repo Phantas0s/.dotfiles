634a41b6214d53a82c1782dcf547d788604fa836
matthieu <matthieu.cneude@gmail.com>
2018-05-10 17:17:56 +0200

    Little changes

--- install/install-from-cloud.sh
+++ install/install-from-cloud.sh
@@ -21,20 +21,18 @@ then
             mkdir $HOME/.ssh > /dev/null
         fi
 
-        echo -e ""
+        echo -e "\n"
         echo -e "${red}Delete the git config..."
         rm $HOME/.gitconfig &>/dev/null
         echo -e "${green}...done!"
 
-        echo -e ""
+        echo -e "\n"
         echo -e "${blue} Installing ssh config from the cloud"
         cp $DOTFILES_CLOUD/openssh/config $HOME/.ssh/config
         chown $USER:$USER $HOME/.ssh/config
         chmod 700 $HOME/.ssh/config
         echo -e "${green} ...done"
     fi
-
-        echo -e "${green} ...done"
 fi
 
 if [ ! -z "$WALLPAPER_PATH" ]
