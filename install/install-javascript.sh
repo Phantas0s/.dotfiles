ebd354190b698884f49bbf87f9e13a4ccc1641e6
matthieu <matthieu.cneude@gmail.com>
2018-06-14 15:30:30 +0200

    Add eslint and babel-eslint

--- install/install-javascript.sh
+++ install/install-javascript.sh
@@ -5,3 +5,17 @@ echo -e "${green}...done!\n"
 
 ln -s ~/$ROOT_CONFIG/javascript/.tern-project ~/
 ln -s ~/$ROOT_CONFIG/javascript/.eslintrc.json ~/
+
+echo -e "${red}Install javascript linters..."
+# Works better than eslint for React
+if [ ! -d ~/.node_modules/lib/node_modules/babel-eslint ];
+then
+    npm i -g babel-eslint
+fi
+
+if [ ! -f ~/.node_modules/bin/eslint ];
+then
+   npm i -g eslint
+fi
+
+echo -e "${green}...done!\n"
