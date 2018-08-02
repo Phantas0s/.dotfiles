7f58e9c07340510542a5a7823a8fe31f17359534
matthieu <matthieu.cneude@gmail.com>
2018-08-02 21:24:33 +0200

    Fix install javascript

--- install/install-javascript.sh
+++ install/install-javascript.sh
@@ -5,19 +5,25 @@ rm $HOME/.tern-project &>/dev/null
 rm $HOME/.eslintrc.json &>/dev/null
 echo -e "${green}...done!\n"
 
-ln -s $DOTFILES/javascript/.tern-project ~/
-ln -s $DOFTILES/javascript/.eslintrc.json ~/
 
-echo -e "${red}Install javascript linters..."
+echo -e "${blue}Install javascript linters..."
 # Works better than eslint for React
 if [ ! -d ~/.node_modules/lib/node_modules/babel-eslint ];
 then
     npm i -g babel-eslint
 fi
 
+if [ ! -d ~/.node_modules/lib/node_modules/eslint-plugin-html ];
+then
+    npm i -g eslint-plugin-html
+fi
+
 if [ ! -f ~/.node_modules/bin/eslint ];
 then
    npm i -g eslint
 fi
+
+ln -s $DOTFILES/javascript/.tern-project $HOME
+ln -s $DOTFILES/javascript/.eslintrc.json $HOME
 
 echo -e "${green}...done!\n"
