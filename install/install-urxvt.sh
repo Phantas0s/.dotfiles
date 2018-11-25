0ce97ec2c3aadc8f034f4832d8ef39e52e7ac26a
Matthieu <matthieu.cneude@gmail.com>
2018-11-25 13:16:54 +0100

    Improve urxvt tab extension

--- install/install-urxvt.sh
+++ install/install-urxvt.sh
@@ -1,12 +1,4 @@
 echo -e "${blue} Installing urxvt config..."
 ln -s $DOTFILES/urxvt/Xdefaults $HOME/.Xdefaults
 ln -s $DOTFILES/urxvt/dircolors $HOME/.dircolors
-rm -rf $HOME/.urxvt/tabbed
-
-if [ ! -d $HOME/.urxvt ]
-  then
-    mkdir $HOME/.urxvt
-fi
-
-ln -sf $DOTFILES/urxvt/extensions/tabbed $HOME/.urxvt/tabbed
 echo -e "${green} ...done"
