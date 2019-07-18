54322253fe91158b8b64ea30776b13fcd65c38ae
matthieu <matthieu.cneude@gmail.com>
2019-07-18 12:50:36 +0200

    Small improvement + change lock screen

---
+++ i3/scripts/lock.sh
@@ -0,0 +1,11 @@
+icon="$HOME/.i3/lock.png"
+img=/tmp/i3lock.png
+
+scrot $img
+convert $img -scale 10% -scale 1000% $img
+
+# Blur image
+#convert $img -blur 0x4 500% $img
+# convert $img $icon -gravity center -composite $img
+
+i3lock -u -i $img
--- install/install-i3.sh
+++ install/install-i3.sh
@@ -9,6 +9,7 @@ if [ ! -d $HOME/.i3 ]
     mkdir $HOME/.i3
 fi
 
+ln -s $DOTFILES/i3/lock.png /$HOME/.i3/lock.png
 ln -s $DOTFILES/i3/config /$HOME/.i3/config
 ln -s $DOTFILES/i3/i3status.conf $HOME/.i3/i3status.conf
 ln -s $DOTFILES/i3/scripts/ $HOME/.i3/scripts
