4d411e77cc20e4c3c420f88916e75a9c6cac3a6b
matthieu <matthieu.cneude@gmail.com>
2019-08-29 15:58:08 +0200

    Add CPU log script

---
+++ zsh/debug/cpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# save in ps.log the 10 processes which are the most CPU intensives, in order to debug server hang
+while true; do (echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail) >> ps.log; sleep 60; done
