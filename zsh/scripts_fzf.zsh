c5bb7c7b263ad85a456e07676067fbb662525994
Matthieu <matthieu.cneude@gmail.com>
2019-11-05 13:12:02 +0100

    Small change for better modularity

--- zsh/scripts_fzf.zsh
+++ zsh/scripts_fzf.zsh
@@ -56,3 +56,45 @@ fmux() {
 
     tmuxp load $prj
 }
+
+# ftmuxp - propose every possible tmuxp session
+ftmuxp() {
+    if [[ ! -n $TMUX ]]; then
+        # get the IDs
+        ID="`ls $HOME/.tmuxp | sed -e 's/\.yml$//'`"
+        if [[ -z "$ID" ]]; then
+            tmux new-session
+        fi
+        create_new_session="Create New Session"
+        ID="${create_new_session}\n$ID"
+        ID="`echo $ID | fzf | cut -d: -f1`"
+        if [[ "$ID" = "${create_new_session}" ]]; then
+            tmux new-session
+        elif [[ -n "$ID" ]]; then
+            tmuxp load "$ID"
+        else
+            :  # Start terminal normally
+        fi
+    fi
+}
+
+# ftmux - propose running tmux session
+ftmux() {
+    if [[ ! -n $TMUX ]]; then
+        # get the IDs
+        ID="`tmux list-sessions`"
+        if [[ -z "$ID" ]]; then
+            tmux new-session
+        fi
+        create_new_session="Create New Session"
+        ID="$ID\n${create_new_session}:"
+        ID="`echo $ID | fzf | cut -d: -f1`"
+        if [[ "$ID" = "${create_new_session}" ]]; then
+            tmux new-session
+        elif [[ -n "$ID" ]]; then
+            tmux attach-session -t "$ID"
+        else
+            :  # Start terminal normally
+        fi
+    fi
+}
