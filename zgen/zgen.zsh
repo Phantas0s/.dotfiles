4cb0e1b3da39cb90cd2e72948b729f3844cf3a5c
Matthieu <matthieu.cneude@gmail.com>
2019-12-11 08:38:24 +0100

    Try out zgen

--- zgen/zgen.zsh
+++ zgen/zgen.zsh
@@ -1,30 +1,53 @@
 
 ### zgen
 ZGEN_RESET_ON_CHANGE=(${HOME}/.zshrc ${SCRIPT_PATH}/zgen/zgen.zsh)
+# load zgen
+source "${HOME}/.zgen/zgen.zsh"
 
 # if the init scipt doesn't exist
 if ! zgen saved; then
-echo "Creating a zgen save"
-    # prezto options
-    zgen prezto editor key-bindings 'vi'
-    zgen prezto prompt theme 'sorin'
+  echo "Creating a zgen save"
 
-    # prezto and modules
-    zgen prezto
-    zgen prezto history
-    zgen prezto directory
-    zgen prezto utility
-    zgen prezto git
-    zgen prezto history-substring-search
-    zgen prezto completion
-    zgen prezto command-not-found
-    zgen prezto syntax-highlighting
+  # Disable loading default Prezto modules
+  # This is necessary due to a quirk of zgen that will load these plugins after
+  # our custom plugins, overwriting our customizations
+  # https://github.com/tarjoilija/zgen/issues/74
+  export ZGEN_PREZTO_LOAD_DEFAULT=0
 
+  # prezto options
+  zgen prezto editor key-bindings 'vi'
+  zgen prezto prompt theme 'thanatos'
 
-    # completions
-    zgen load zsh-users/zsh-completions src
+  # prezto and modules
+  zgen prezto
 
-    # save all to init script
-    zgen save
+  # Default plugins
+  zgen load sorin-ionescu/prezto modules/environment
+  zgen load sorin-ionescu/prezto modules/terminal
+  zgen load sorin-ionescu/prezto modules/editor
+  zgen load sorin-ionescu/prezto modules/history
+  zgen load sorin-ionescu/prezto modules/directory
+  # zgen load sorin-ionescu/prezto modules/spectrum
+  zgen load sorin-ionescu/prezto modules/utility
+  zgen load sorin-ionescu/prezto modules/completion
+  zgen load sorin-ionescu/prezto modules/git
+  # zgen load sorin-ionescu/prezto modules/prompt
+
+  # Extra plugins
+  # zgen load sorin-ionescu/prezto modules/history-substring-search
+
+  # 3rd Party plugins
+  zgen load robbyrussell/oh-my-zsh plugins/docker
+  zgen load zsh-users/zsh-autosuggestions
+  zgen load zsh-users/zsh-completions
+
+  zgen prezto command-not-found
+
+  zgen load unixorn/autoupdate-zgen
+  zgen load zsh-users/zsh-syntax-highlighting
+  # zgen load denysdovhan/spaceship-prompt spaceship
+  zgen load chauncey-garrett/zsh-prompt-garrett garrett
+
+  # save all to init script
+  zgen save
 fi
-
