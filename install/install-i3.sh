63165094b36893b2c8222876bca46fb1bebea7ab
Matthieu Cneude <matthieu.cneude@gmail.com>
2019-12-21 10:12:46 +0100

    Add small i3bar for small screen

--- install/install-i3.sh
+++ install/install-i3.sh
@@ -11,5 +11,10 @@ fi
 
 ln -s $DOTFILES/i3/lock.png /$HOME/.i3/lock.png
 ln -s $DOTFILES/i3/config /$HOME/.i3/config
-ln -s $DOTFILES/i3/i3status.conf $HOME/.i3/i3status.conf
 ln -s $DOTFILES/i3/scripts/ $HOME/.i3/scripts
+
+if [[ $I3_STATUS_BAR == "small" ]]; then
+    ln -s $DOTFILES/i3/i3status-small.conf $HOME/.i3/i3status.conf
+else
+    ln -s $DOTFILES/i3/i3status.conf $HOME/.i3/i3status.conf
+fi
