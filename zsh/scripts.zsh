881c0cff3c5e27fb9605fdf5cf92380600db60d3
Matthieu Cneude <matthieu.cneude@gmail.com>
2019-12-28 16:30:35 +0100

    Add rar extraction

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -31,13 +31,13 @@ extract() {
       *.tar.bz2)  tar xjf $1      ;;
       *.tar.gz)   tar xzf $1      ;;
       *.bz2)      bunzip2 $1      ;;
-      *.rar)      rar x $1        ;;
       *.gz)       gunzip $1       ;;
       *.tar)      tar xf $1       ;;
       *.tbz2)     tar xjf $1      ;;
       *.tgz)      tar xzf $1      ;;
       *.zip)      unzip $1        ;;
       *.7z)       7z x $1         ;; # require p7zip
+      *.rar)      7z x $1         ;; # require p7zip
       *.iso)      7z x $1         ;; # require p7zip
       *.Z)        uncompress $1   ;;
       *)          echo "'$1' cannot be extracted" ;;
