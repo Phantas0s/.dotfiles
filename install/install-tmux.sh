297de2540ea3d2a0db1f0275fba1deeafbe6f277
matthieu <matthieu.cneude@gmail.com>
2020-04-03 21:02:01 +0200

    Add automatic installation of tmux plugin manager if not installed

--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -6,11 +6,17 @@ echo "${blue}Install tmux config..."
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 
 
+
 if [ ! -d $HOME/.tmux ]
   then
     mkdir $HOME/.tmux
     git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
     $HOME/.tmux/plugins/tpm/bin/install_plugins
+fi
+
+if [ ! -d $HOME/.tmux/plugins/tmp ]
+then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 echo "${green} ...done"
