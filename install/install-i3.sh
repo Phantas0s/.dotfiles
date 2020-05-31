6e0faeb3b469d1fa9a21f340980feb6150f1c7d8
matthieu <matthieu.cneude@gmail.com>
2020-05-31 10:56:43 +0200

    Create new env I3_CONFIG

--- install/install-i3.sh
+++ install/install-i3.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-rm -rf $XDG_CONFIG_HOME/i3/ &>/dev/null
-rm -rf $XDG_CONFIG_HOME/.i3/ &>/dev/null
+rm -rf $I3_CONFIG/ &>/dev/null
 
-if [ ! -d $XDG_CONFIG_HOME/.i3 ]
+if [ ! -d $I3_CONFIG/ ]
   then
-    mkdir $XDG_CONFIG_HOME/.i3
+    mkdir $I3_CONFIG/
 fi
 
-ln -s $DOTFILES/i3/lock.png /$XDG_CONFIG_HOME/.i3/lock.png
-ln -s $DOTFILES/i3/config /$XDG_CONFIG_HOME/.i3/config
-ln -s $DOTFILES/i3/scripts/ $XDG_CONFIG_HOME/.i3/scripts
+ln -s $DOTFILES/i3/lock.png $I3_CONFIG/lock.png
+ln -s $DOTFILES/i3/config $I3_CONFIG/config
+ln -s $DOTFILES/i3/scripts/ $I3_CONFIG/scripts
 
 if [[ $I3_STATUS_BAR == "small" ]]; then
-    ln -s $DOTFILES/i3/i3status-small.conf $XDG_CONFIG_HOME/.i3/i3status.conf
+    ln -s $DOTFILES/i3/i3status-small.conf $I3_CONFIG/i3status.conf
 else
-    ln -s $DOTFILES/i3/i3status.conf $XDG_CONFIG_HOME/.i3/i3status.conf
+    ln -s $DOTFILES/i3/i3status.conf $I3_CONFIG/i3status.conf
 fi
