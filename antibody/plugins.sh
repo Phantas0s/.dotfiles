e976e0abe94e68ab646d22f5ee14337a2414c5ea
matthieu <matthieu.cneude@gmail.com>
2020-06-15 16:58:21 +0200

    Get rid of antibody

--- antibody/plugins.sh
+++
@@ -1,4 +0,0 @@
-source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Tarrasch-SLASH-zsh-bd/bd.plugin.zsh
-fpath+=( $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Tarrasch-SLASH-zsh-bd )
-source $XDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( $xDG_CACHE_HOME/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
