83404ca426de8d99ba5c1abad55b1d114b00ca69
matthieu <matthieu.cneude@gmail.com>
2020-06-23 10:49:15 +0200

    Copy the history instead of symbolic link (useless anyway, zsh will always copy back from a temp file)

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -5,7 +5,7 @@ rm -rf $ZDOTDIR &>/dev/null
 
 mkdir $ZDOTDIR;
 
-[ -f "$DOTFILES_CLOUD/zsh/.zhistory" ] && ln -sf $DOTFILES_CLOUD/zsh/.zhistory $ZDOTDIR/.zhistory;
+[ -f "$DOTFILES_CLOUD/zsh/.zhistory" ] && cp $DOTFILES_CLOUD/zsh/.zhistory $ZDOTDIR/.zhistory;
 
 ln -s -f $DOTFILES/zsh/zshenv $HOME/.zshenv
 ln -s -f $DOTFILES/zsh/zshrc $ZDOTDIR/.zshrc
