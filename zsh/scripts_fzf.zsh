be967a8da4c0144ba200b86e3ac6e23327339cfa
matthieu <matthieu.cneude@gmail.com>
2020-06-26 13:01:16 +0200

    Bring syntax highlighting zsh plugin at the end

--- zsh/scripts_fzf.zsh
+++ zsh/scripts_fzf.zsh
@@ -59,23 +59,27 @@ fmux() {
 
 # ftmuxp - propose every possible tmuxp session
 ftmuxp() {
-    if [[ ! -n $TMUX ]]; then
-        # get the IDs
-        ID="`ls $XDG_CONFIG_HOME/tmuxp | sed -e 's/\.yml$//'`"
-        if [[ -z "$ID" ]]; then
-            tmux new-session
-        fi
-        create_new_session="Create New Session"
-        ID="${create_new_session}\n$ID"
-        ID="`echo $ID | fzf | cut -d: -f1`"
-        if [[ "$ID" = "${create_new_session}" ]]; then
-            tmux new-session
-        elif [[ -n "$ID" ]]; then
-            printf '\033]777;tabbedx;set_tab_name;%s\007' "$ID"
-            tmuxp load "$ID"
-        else
-            :  # Start terminal normally
-        fi
+    if [[ -n $TMUX ]]; then
+        return
+    fi
+
+    # get the IDs
+    ID="`ls $XDG_CONFIG_HOME/tmuxp | sed -e 's/\.yml$//'`"
+    if [[ -z "$ID" ]]; then
+        tmux new-session
+    fi
+
+    local create_new_session="Create New Session"
+
+    ID="${create_new_session}\n$ID"
+    ID="`echo $ID | fzf | cut -d: -f1`"
+
+    if [[ "$ID" = "${create_new_session}" ]]; then
+        tmux new-session
+    elif [[ -n "$ID" ]]; then
+        # Change name of urxvt tab to session name
+        printf '\033]777;tabbedx;set_tab_name;%s\007' "$ID"
+        tmuxp load "$ID"
     fi
 }
 
