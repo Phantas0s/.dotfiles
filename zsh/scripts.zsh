458399711c29b533f9eac869962d111dee3bd6cb
matthieu <matthieu.cneude@gmail.com>
2020-06-27 19:35:17 +0200

    Change sample bitrate for yeti microphone

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -23,7 +23,7 @@ screencast() {
     if [ ! -z $1 ]; then
        [ $screen -eq 1 ] && offset="+0,$(( $bigger_height -  $(screenres 1 | awk -Fx '{print $2}')))"
        [ $screen -eq 2 ] && offset="+$(screenres 1 | awk -Fx '{print $1}')"
-       ffmpeg -f x11grab -s $(screenres $screen) -i :0.0$offset -f pulse -i default -c:v libx264 -preset ultrafast -c:a aac $1
+       ffmpeg -f x11grab -s $(screenres $screen) -i :0.0$offset -f pulse -sample_rate 44100 -i default -c:v libx264 -preset ultrafast -c:a aac $1
 
        # Other codecs
        # -c:v ffvhuff   # lossless but HUGE
