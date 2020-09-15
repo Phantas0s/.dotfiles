d04733ae7c517bf426d2fbb995c2a2081b5cb1f5
matthieu <matthieu.cneude@gmail.com>
2020-09-15 16:08:40 +0200

    Get rid of gitlab and add very important double quote to the script

--- install/install-projects.sh
+++ install/install-projects.sh
@@ -2,59 +2,54 @@
 # The ssh key have to be created manually of course...
 # Only Phantas0s have access to the repo via SSH :D
 
-mkdir -p $GOPATH/src/github.com/Phantas0s > /dev/null
+mkdir -p "$GOPATH/src/github.com/Phantas0s" > /dev/null
 
-if [ -f $HOME/.ssh/gitlab ];
-then
-    if [ ! -d $HOME/workspace/webtechno ]; then
+if [ -f "$HOME/.ssh/github" ]; then
+    if [ ! -d "$HOME/workspace/webtechno" ]; then
         CURRENT=$(pwd)
 
-        cd $HOME/workspace
-        git clone --recursive git@gitlab.com:Phantas0s/webtechno.git
-        cd $HOME/workspace/webtechno
-        if [ -d $HOME/workspace/webtechno/githooks ]; then
+        cd "$HOME/workspace"
+        mkdir "$HOME/workspace/webtechno"
+        git clone --recursive git@github.com:Phantas0s/the_valuable_dev.git "$HOME/workspace/webtechno"
+        cd "$HOME/workspace/webtechno"
+        if [ -d "$HOME/workspace/webtechno/githooks" ]; then
             cd .git/hooks/
             ln -s ../../githooks/* .
         fi
-        cd $CURRENT
+        cd "$CURRENT"
     fi
-else
-    dot_mes_warn "Please generate a 'gitlab' ssh-key"
-fi
-
-if [ -f $HOME/.ssh/github ]; then
-    if [ ! -d $HOME/workspace/ottosocial ]; then
-        cd $HOME/workspace
+    if [ ! -d "$HOME/workspace/ottosocial" ]; then
+        cd "$HOME/workspace"
         git clone git@github.com:Phantas0s/ottosocial.git
         go get ./ottosocial
         go install ./ottosocial
         cd -
     fi
 
-    if [ ! -d $HOME/workspace/devdash ]; then
-        cd $HOME/workspace
+    if [ ! -d "$HOME/workspace/devdash" ]; then
+        cd "$HOME/workspace"
         git clone git@github.com:Phantas0s/devdash.git
         go get ./devdash
         go install ./devdash
         cd -
     fi
 
-    if [ ! -d $GOPATH/src/github.com/Phantas0s/testomatic ]; then
-        cd $GOPATH/src/github.com/Phantas0s
+    if [ ! -d "$GOPATH/src/github.com/Phantas0s/testomatic" ]; then
+        cd "$GOPATH/src/github.com/Phantas0s"
         git clone git@github.com:Phantas0s/testomatic.git
         go get ./testomatic
         go install ./testomatic
         cd -
     fi
 
-    if [ ! -d $HOME/workspace/playground ]; then
-        cd $HOME/workspace
+    if [ ! -d "$HOME/workspace/playground" ]; then
+        cd "$HOME/workspace"
         git clone git@github.com:Phantas0s/playground.git
         cd -
     fi
 
-    if [ ! -d $HOME/workspace/ArchInstall ]; then
-        cd $HOME/workspace
+    if [ ! -d "$HOME/workspace/ArchInstall" ]; then
+        cd "$HOME/workspace"
         git clone git@github.com:Phantas0s/ArchInstall.git
         cd -
     fi
