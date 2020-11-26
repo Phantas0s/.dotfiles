6c34a5a77c0b4ba4847fe2f21e0374244a322364
matthieu <matthieu.cneude@gmail.com>
2020-11-26 20:39:14 +0100

    Install icons

--- install.sh
+++ install.sh
@@ -77,3 +77,4 @@ dot_is_installed mpd && dot_is_installed ncmpcpp && dot_install mpd
 dot_is_installed gimp && dot_install gimp
 dot_is_installed zathura && dot_install zathura
 dot_is_installed doublecmd && dot_install doublecmd
+dot_install gtk
---
+++ install/install-gtk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# For more icons and themes, see:
+# https://www.gnome-look.org/
+# https://aur.archlinux.org/packages.php?O=0&L=0&C=17&K=icon-theme&SeB=nd&SB=n&SO=a&PP=50&do_Search=Go
+# https://wiki.archlinux.org/index.php/GTK
+
+mkdir -p "$HOME/.icons"
+
+if [ ! -d "$HOME/.icons/Flatery-Yellow" ]; then
+    rm -rf /tmp/Flatery
+    git clone https://github.com/cbrnix/Flatery /tmp/Flatery
+    mv /tmp/Flatery/Flatery-Yellow "$HOME/.icons"
+fi
