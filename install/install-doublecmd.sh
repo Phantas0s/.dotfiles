af086aaf65efc3279cee8a7a2a25c272dd2e5773
matthieu <matthieu.cneude@gmail.com>
2020-11-26 19:06:38 +0100

    Delete useless files for doublecommander

--- install/install-doublecmd.sh
+++ install/install-doublecmd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-rm -rf "$XDG_CONFIG_HOME/doublecmd"
-ln -sf "$DOTFILES/doublecmd" "$XDG_CONFIG_HOME/doublecmd"
+mkdir -p "$XDG_CONFIG_HOME/doublecmd"
+ln -sf "$DOTFILES/doublecmd/multiarc.ini" "$XDG_CONFIG_HOME/doublecmd/multiarc.ini"
+ln -sf "$DOTFILES/doublecmd/session.ini" "$XDG_CONFIG_HOME/doublecmd/session.ini"
+ln -sf "$DOTFILES/doublecmd/shortcuts.scf" "$XDG_CONFIG_HOME/doublecmd/shortcuts.scf"
