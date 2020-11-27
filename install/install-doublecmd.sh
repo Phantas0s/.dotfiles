430c465a292532f5db1cf01e10a8708aaf399c00
matthieu <matthieu.cneude@gmail.com>
2020-11-27 11:14:02 +0100

    Tweak install of double commander

--- install/install-doublecmd.sh
+++ install/install-doublecmd.sh
@@ -2,5 +2,4 @@
 
 mkdir -p "$XDG_CONFIG_HOME/doublecmd"
 ln -sf "$DOTFILES/doublecmd/multiarc.ini" "$XDG_CONFIG_HOME/doublecmd/multiarc.ini"
-ln -sf "$DOTFILES/doublecmd/session.ini" "$XDG_CONFIG_HOME/doublecmd/session.ini"
 ln -sf "$DOTFILES/doublecmd/shortcuts.scf" "$XDG_CONFIG_HOME/doublecmd/shortcuts.scf"
