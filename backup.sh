065cb227299b08140ea08905ec21891dd4a00c43
matthieu <matthieu.cneude@gmail.com>
2020-12-14 08:25:38 +0100

    backup emulators

--- backup.sh
+++ backup.sh
@@ -21,6 +21,7 @@ backup() {
         $HOME/Games/scummvm home/Games/scummvm
         $HOME/Games/internet home/Games/internet
         $HOME/Games/dosbox home/Games/dosbox
+        $HOME/Games/emulators home/Games/emulators
         $MEDIA/assets assets
         $MEDIA/Documentaries documentaries
         $MEDIA/Ebooks ebooks
