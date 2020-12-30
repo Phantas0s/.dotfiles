56cd3926da940c09bfe828077f9fea059082db5a
matthieu <matthieu.cneude@gmail.com>
2020-12-30 08:34:19 +0100

    Improve deadbeef synchronization (playlists are now on cloud)

--- install/install-deadbeef.sh
+++ install/install-deadbeef.sh
@@ -1,2 +1,9 @@
+# Plugins
 rm -rf "$HOME/.local/lib/deadbeef" && ln -sf "$DOTFILES/deadbeef/plugins" "$HOME/.local/lib/deadbeef"
-rm -rf "$XDG_CONFIG_HOME/deadbeef" && ln -sf "$DOTFILES/deadbeef/config" "$XDG_CONFIG_HOME/deadbeef"
+
+# Config
+ln -sf "$DOTFILES/deadbeef/config/config" "$XDG_CONFIG_HOME/deadbeef/config"
+ln -sf "$DOTFILES/deadbeef/config/dspconfig" "$XDG_CONFIG_HOME/deadbeef/dspconfig"
+
+# Playlists
+rm -rf "$XDG_CONFIG_HOME/deadbeef/playlists" && ln -sf "$CLOUD/multimedia/music/deadbeef/playlists" "$XDG_CONFIG_HOME/deadbeef/playlists"
