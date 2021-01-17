312fb371f37e68fecb8f92d96adb5e10a94142e7
matthieu <matthieu.cneude@gmail.com>
2021-01-17 08:35:37 +0100

    Improve config

--- install/install-git.sh
+++ install/install-git.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rm $DOTFILES/git/gitconfig >/dev/null
-cp $DOTFILES/git/gitconfig_template $DOTFILES/git/gitconfig
+rm "$DOTFILES/git/gitconfig >/dev/null"
+cp "$DOTFILES/git/gitconfig_template" "$DOTFILES/git/gitconfig"
 
 if [ -z "$GIT_EMAIL" ]
   then
     echo "Git config - Please enter your email address \n"
     echo "> "
     read GIT_EMAIL
-    echo "GIT_EMAIL=$GIT_EMAIL" >> $DOTFILES/install_config
+    echo "GIT_EMAIL=$GIT_EMAIL" >> "$DOTFILES/install_config"
 fi
 
 sed -i -e "s/<email>/${GIT_EMAIL}/g" $DOTFILES/git/gitconfig
@@ -18,15 +18,15 @@ if [ -z "$GIT_USER" ]
     echo "Git config - Please enter your name \n"
     echo "> "
     read GIT_USER
-    echo "GIT_USER=$GIT_USER" >> $DOTFILES/install_config
+    echo "GIT_USER=$GIT_USER" >> "$DOTFILES/install_config"
 fi
 
-sed -i -e "s/<name>/${GIT_USER}/g" $DOTFILES/git/gitconfig
+sed -i -e "s/<name>/${GIT_USER}/g" "$DOTFILES/git/gitconfig"
 
-[ ! -d $XDG_CONFIG_HOME/git/config ] && mkdir -p $XDG_CONFIG_HOME/git/
+[ ! -d "$XDG_CONFIG_HOME/git/config" ] && mkdir -p "$XDG_CONFIG_HOME/git"
 
-ln -sf $DOTFILES/git/gitconfig $XDG_CONFIG_HOME/git/config
-ln -sf $DOTFILES/git/gitignore $XDG_CONFIG_HOME/git/ignore
+ln -sf "$DOTFILES/git/gitconfig" "$XDG_CONFIG_HOME/git/config"
+ln -sf "$DOTFILES/git/gitignore" "$XDG_CONFIG_HOME/git/ignore"
 
 # Set the global hooks
 git config --global init.templatedir "$DOTFILES/git/templates"
