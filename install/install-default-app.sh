6c863a81184492bcc5c131b876de705095fe4515
matthieu <matthieu.cneude@gmail.com>
2021-04-25 16:34:02 +0200

    Add urls to newsboat

---
+++ install/install-default-app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf "$XDG_DATA_HOME/applications"
+ln -sf "$DOTFILES/default_app/applications" "$XDG_DATA_HOME"
+ln -sf "$DOTFILES/default_app/mimeapps.list" "$XDG_CONFIG_HOME/mimeapps.list"
