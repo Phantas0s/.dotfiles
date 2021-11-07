9407844ec07688a952aa927c1782f57563c348eb
matthieu <matthieu.cneude@gmail.com>
2021-11-07 07:33:10 +0100

    Improve install fonts

--- install/install-fonts.sh
+++ install/install-fonts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # see https://github.com/ryanoasis/nerd-fonts#patched-fonts for many fonts
 
-ln -sf "$DOTFILES/fonts/fonts" "$XDG_DATA_HOME"
+[ ! -d "$DOTFILES/fonts/fonts" ] && ln -s "$DOTFILES/fonts/fonts" "$XDG_DATA_HOME"
 ln -sf "$DOTFILES/fonts/fontconfig" "$XDG_CONFIG_HOME"
