c4a8f6027fcf01a22ddaf7edadd051a0f869f14c
matthieu <matthieu.cneude@gmail.com>
2021-11-26 10:06:39 +0100

    Continue lua exploration with Neovim

--- nvim/lua/custom/delete_all_buffer.lua
+++ nvim/lua/custom/delete_all_buffer.lua
@@ -1,24 +1,26 @@
 #!/usr/bin/env lua
 
-local function buflist()
-    local list = {}
-    vim.cmd([[
-        redir => bufnames
-        silent ls
-        redir END
-    ]])
+local function split(str,div) -- credit: http://richard.warburton.it
+  if (div=='') then return false end
+  local pos,arr = 0,{}
+  -- for each divider found
+  for st,sp in function() return string.find(str,div,pos,true) end do
+    table.insert(arr,string.sub(str,pos,st-1)) -- Attach chars left of current divider
+    pos = sp + 1 -- Jump past current divider
+  end
+  table.insert(arr,string.sub(str,pos)) -- Attach chars right of last divider
+  return arr
+end
 
+local function buflist()
     bufnames = vim.api.nvim_exec('silent ls', true)
-    print(vim.inspect(bunames))
-    local bufs = vim.fn.split(bufnames, "\n")
-    print(vim.inspect(bufs))
+    local list, bufs = {}, vim.fn.split(bufnames, "\n")
     for k, v in pairs(bufs) do
-        local buf = vim.fn.split(v, '"')
-        vim.fn.add(list, buf[-2])
+        local buf = split(v, '"')
+        table.insert(list, buf[2])
+        print(vim.inspect(list))
     end
 
-    print('lala')
-    vim.inspect(list)
     return list
 end
 buflist()
