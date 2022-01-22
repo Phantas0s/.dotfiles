8f655f92398c98a06bb27b3e400a3325d1abe0ee
matthieu <matthieu.cneude@gmail.com>
2022-01-22 08:09:26 +0100

    Fix bat

--- install/install-bat.sh
+++ install/install-bat.sh
@@ -4,5 +4,6 @@ BAT_CONFIG_DIR=$(bat --config-dir)
 rm -rf "$BAT_CONFIG_DIR"
 
 mkdir -p "$BAT_CONFIG_DIR"
-ln -sf "$DOTFILES/bat/*" "${BAT_CONFIG_DIR}/"
+ln -s "$DOTFILES/bat/config" "${BAT_CONFIG_DIR}"
+ln -s "$DOTFILES/bat/themes" "${BAT_CONFIG_DIR}"
 bat cache --build
