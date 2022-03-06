0cf41ac753bcf4dc29e3586f05c7fac4abe0fe71
Matthieu <matthieu.cneude@gmail.com>
2022-03-06 15:55:17 +0100

    Minor changes

--- zsh/scripts_fzf.zsh
+++ zsh/scripts_fzf.zsh
@@ -4,14 +4,6 @@
 # | Git |
 # +-----+
 
-# checkout git branch (including remote branches) with FZF
-fgco() {
-  local branches=$(git branch --all | grep -v HEAD) &&
-  local branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-
 # git log browser with FZF
 fglog() {
   git log --graph --color=always \
@@ -22,35 +14,6 @@ fglog() {
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
                 {}
 FZF-EOF"
-}
-
-# fstash - easier way to deal with stashes
-# type fstash to get a list of your stashes
-# enter shows you the contents of the stash
-# ctrl-d shows a diff of the stash against your current HEAD
-# ctrl-b checks the stash out as a branch, for easier merging
-fstash() {
-  local out q k sha
-  while out=$(
-    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-    fzf --ansi --no-sort --query="$q" --print-query \
-        --expect=ctrl-d,ctrl-b);
-  do
-    mapfile -t out <<< "$out"
-    q="${out[0]}"
-    k="${out[1]}"
-    sha="${out[-1]}"
-    sha="${sha%% *}"
-    [[ -z "$sha" ]] && continue
-    if [[ "$k" == 'ctrl-d' ]]; then
-      git diff $sha
-    elif [[ "$k" == 'ctrl-b' ]]; then
-      git stash branch "stash-$sha" $sha
-      break;
-    else
-      git stash show -p $sha
-    fi
-  done
 }
 
 # +--------+
@@ -132,7 +95,13 @@ fdot() {
 
 # List projects
 fwork() {
-    cd ~/workspace/$(find ~/workspace/* -type d -prune -exec basename {} ';' | sort | uniq | nl | fzf | cut -f 2)
+    result=$(find ~/workspace/* -type d -prune -exec basename {} ';' | sort | uniq | nl | fzf | cut -f 2)
+    [ -n "$result" ] && cd ~/workspace/$result
+}
+
+fpdf() {
+    result=$(find -type f -name '*.pdf' | nl | fzf | cut -f 2)
+    [ -n "$result" ] && zathura "$result" &
 }
 
 # List mindmaps
