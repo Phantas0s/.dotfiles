e0bbc1469b2c374f2665f3b7e3927734ce719952
Matthieu <matthieu.cneude@gmail.com>
2022-03-26 17:23:13 +0100

    Make my i3/tmux scratchpad as a script
    * Allow to recreate it easily if I close it
    * Definition of the scratchpad only at one place

---
+++ bash/scripts/scratchpad.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminal scratchpad with tmux and three windows:
+# 1.term
+# 2.vim (on a temp file)
+# 3.translation CLI
+# Used to restart i3 scratchpad if it was closed; logic is duplicated in i3/config
+
+tmux kill-session -t scratchpad
+urxvtc -name urxvt_scratchpad -e tmux new-session -d -s scratchpad ';' \
+    new-window -n nvim 'nvim +e /tmp/scratchpad.md +"set spelllang=de,en"' ';' \
+    new-window -n trans 'trans -I' ';' \
+    attach-session -d -t scratchpad ';'
