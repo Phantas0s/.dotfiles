6db47a4874ca2264536acfc0387931c15824a7d5
Matthieu <matthieu.cneude@gmail.com>
2023-02-08 10:03:24 +0100

    More fzf goodies

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,15 +1,21 @@
 #!/usr/bin/env zsh
 
-source /usr/share/fzf/completion.zsh
 source /usr/share/fzf/key-bindings.zsh
 
-# zle     -N            fzf-cd-widget
-# bindkey -M emacs '^e' fzf-cd-widget
-# bindkey -M vicmd '^e' fzf-cd-widget
-# bindkey -M viins '^e' fzf-cd-widget
+# Rebind ALT-c to CTRL-e
+bindkey -rM emacs '\ec'
+bindkey -rM vicmd '\ec'
+bindkey -rM viins '\ec'
 
-zle     -N   fzf-completion
-bindkey '^e' fzf-completion
+zle     -N            fzf-cd-widget
+bindkey -M emacs '^e' fzf-cd-widget
+bindkey -M vicmd '^e' fzf-cd-widget
+bindkey -M viins '^e' fzf-cd-widget
+
+# zle     -N   fzf-completion
+# bindkey '^e' fzf-completion
+
+source /usr/share/fzf/completion.zsh
 
 source $DOTFILES/zsh/scripts_fzf.zsh # fzf Scripts
 # _fzf_comprun() {
