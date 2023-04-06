ef1bf558b64dde677e584ea59c92c258a77c540f
Matthieu <matthieu.cneude@gmail.com>
2023-04-06 08:02:20 +0200

    Small improvements

--- install/install-thunderbird.sh
+++ install/install-thunderbird.sh
@@ -3,5 +3,5 @@
 path=$(find "$HOME/.thunderbird" -path '*yahoo.com' -type d)
 cd "$path" || exit
 rm "$path/msgFilterRules.dat"
-ln -s "$CLOUD/apps/thunderbird/yahoo/msgFilterRules.dat"
-cd -
+ln -s "$CLOUD/apps/thunderbird/yahoo/msgFilterRules.dat" .
+cd - || exit
